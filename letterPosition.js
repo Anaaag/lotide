30f9d3872feb201366123facead41212f51cc6c1
Anaaag <agarc20@outlook.com>
2022-04-08 03:22:46 -0700

    tests passed

---
+++ letterPosition.js
@@ -0,0 +1,21 @@
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++){
+    console.log(sentence)
+  }
+  
+  return results;
+};
+
+
+
+
+
+
+
+
+console.log(letterPositions("cat in the hat"))
+
+
+
