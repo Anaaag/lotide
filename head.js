d497e910e97deb8bbb196cc312c292f5f40865cc
Anaaag <agarc20@outlook.com>
2022-04-11 23:43:22 -0700

    seperating head's test

--- head.js
+++ head.js
@@ -1,20 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(arr) {
   let first = arr[0];
   return first;
 };
 
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]));
+module.exports = head;
 
